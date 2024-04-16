b86d476b37f7e4fff6ce1891c3a1199ce4c71f92
Nilesh Telang <95858066+NileshTelang@users.noreply.github.com>
2024-04-16 09:23:04 +0530

    Update app.js

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 
 //database
-mongoose.connect("mongodb+srv://Nov0:nov0@nov0.klonhmq.mongodb.net/todolistDB",{useNewUrlParser : true});
+mongoose.connect("process.env.MONGO_URI",{useNewUrlParser : true});
 
 //schema
 const itemSchema = {
